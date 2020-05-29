5ae64c13836d67ab33ce89acd4e4665caa75b262
aredra <rnswn48@gmail.com>
2020-05-29 09:55:53 +0900

    Added searchResult view

--- vanilla-search/js/controllers/MainController.js
+++ vanilla-search/js/controllers/MainController.js
@@ -1,4 +1,7 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+
+import SearchModel from '../models/SearchModel.js'
 
 const tag = '[MainController]';
 
@@ -8,13 +11,31 @@ export default {
         FormView.setup(document.querySelector('form'))
             .on('@submit', e=> this.onSubmit(e.detail.input))
             .on('@reset', e=> this.onReset(e));
+
+        ResultView.setup(document.querySelector('#search-result'));
+    },
+
+    search(query) {
+        console.log(tag, 'search()', query);
+        SearchModel.list(query).then(res =>  {
+            this.onSearchResult(res);  
+        })
+        .catch(err => {
+            console.log(err);
+        })
     },
 
     onSubmit(input) {
         console.log(tag, 'onSubmit()', input);
+        this.search(input);
     },
 
     onReset(input) {
         console.log(tag, 'onReset()', input);
+        ResultView.hide();
+    },
+
+    onSearchResult(data) {
+        ResultView.render(data);
     }
 }
---
+++ vanilla-search/js/views/ResultView.js
@@ -0,0 +1,30 @@
+import View from './View.js';
+
+const tag = '[ResultView]';
+const ResultView = Object.create(View);
+
+ResultView.setup = function (el)  {
+    this.init(el);
+}
+
+ResultView.render = function(data = []) {
+    console.log(tag, 'render()', data);
+    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : 'None';
+    this.show();
+}
+
+ResultView.getSearchResultsHtml = function (data) {
+    return data.reduce((html, item) => {
+        html += this.getSearchItemHtml(item);
+        return html;
+    }, '<ul>') + '</ul>';
+}
+
+ResultView.getSearchItemHtml = function (item) {
+  return `<li>
+        <img src="${item.image}">
+        <p>${item.name}</p>
+    </li>`;
+};
+
+export default ResultView
