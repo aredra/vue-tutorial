23ca05db60c536d1eb66b3a458d364f8af648818
aredra <rnswn48@gmail.com>
2020-06-16 22:18:30 +0900

    Setting tabs components

--- vue-search/js/app.js
+++ vue-search/js/app.js
@@ -1,18 +1,38 @@
+import SearchModel from './models/SearchModel.js';
+import HistoryModel from "./models/HistoryModel.js";
+import KeywordModel from "./models/KeywordModel.js";
+
 new Vue({
-    el: '#app',
-    data: {
-        query: ''
+  el: "#app",
+  data: {
+    query: "",
+    searchResult: [],
+    tabs: ["추천 검색어", "최근 검색어"],
+    selectedTab: "",
+    submited: false,
+  },
+  created() {
+    this.selectedTab = this.tabs[0];
+  },
+  methods: {
+    onSubmit(e) {
+      this.search();
     },
-    methods: {
-        onSubmit(e) {
-            
-        },
-        onReset(e) {
-            this.query = ''
-            debugger;
-        },
-        onKeyup(e) {
-            if (!this.query.length) this.onReset();
-        }
-    }
-})
+    onReset(e) {
+      this.query = "";
+      this.submited = false;
+    },
+    onKeyup(e) {
+      if (!this.query.length) this.onReset();
+    },
+    onClickTab(tab) {
+      this.selectedTab = tab;
+    },
+    search() {
+      SearchModel.list().then((data) => {
+        this.searchResult = data;
+      });
+      this.submited = true;
+    },
+  },
+});
